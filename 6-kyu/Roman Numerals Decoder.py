4d2f4b4634f85b89d353cfe0e84787b3424caa07
Pariya <87968419+pariyajebreili@users.noreply.github.com>
2022-07-16 19:07:10 +0430

    Update Roman Numerals Decoder.py

---
+++ 6-kyu/Roman Numerals Decoder.py
@@ -0,0 +1,43 @@
+# https://www.codewars.com/kata/51b6249c4612257ac0000005/train/python
+'''
+Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
+
+Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+
+Example:
+
+solution('XXI') # should return 21
+Help:
+
+Symbol    Value
+I          1
+V          5
+X          10
+L          50
+C          100
+D          500
+M          1,000
+'''
+# 1
+def solution(roman):
+    dict = {
+        "M": 1000,
+        "D": 500,
+        "C": 100,
+        "L": 50,
+        "X": 10,
+        "V": 5,
+        "I": 1
+    }
+
+    last, total = 0, 0
+    for c in list(roman)[::-1]:
+        if last == 0:
+            total += dict[c]
+        elif last > dict[c]:
+            total -= dict[c]
+        else:
+            total += dict[c]
+        last = dict[c]
+    return total
+
