6e40bd0da6ddbe964b5ac4c47b3a61eee2d3536e
pariya <pariyajebreili@gmail.com>
2022-07-19 22:52:55 +0430

    Regex Password Validation

---
+++ 5-kyu/Regex Password Validation.py
@@ -0,0 +1,21 @@
+# https://www.codewars.com/kata/52e1476c8147a7547a000811/train/python
+'''
+You need to write regex that will validate a password to make sure it meets the following criteria:
+
+At least six characters long
+contains a lowercase letter
+contains an uppercase letter
+contains a digit
+only contains alphanumeric characters (note that '_' is not alphanumeric)
+
+'''
+
+regex = (
+    '^'            # start line
+    '(?=.*\d)'     # must contain one digit from 0-9
+    '(?=.*[a-z])'  # must contain one lowercase characters
+    '(?=.*[A-Z])'  # must contain one uppercase characters
+    '[a-zA-Z\d]'   # permitted characters (alphanumeric only)
+    '{6,}'         # length at least 6 chars
+    '$'            # end line
+)
