a59372943400e487f697afc306a3ac70c64bd2df
Pariya <pariyajebreili@gmail.com>
2022-08-02 15:23:02 +0430

    Unique In Order

---
+++ 6-kyu/Unique In Order.py
@@ -0,0 +1,23 @@
+# https://www.codewars.com/kata/54e6533c92449cc251001667/train/python
+'''
+Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+
+unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+unique_in_order([1,2,2,3,3])       == [1,2,3]
+
+'''
+
+# 1
+def unique_in_order(iterable):
+    result = []
+    prev = None
+    for char in iterable[0:]:
+        if char != prev:
+            result.append(char)
+            prev = char
+    return result
+
+
