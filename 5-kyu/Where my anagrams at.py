c19839ea683d3c5546404ebc4e37245fa1d0f9ab
Pariya <pariyajebreili@gmail.com>
2022-08-06 19:21:09 +0430

    Sudoku Solution Validator

--- 5-kyu/Where my anagrams at.py
+++ 5-kyu/Where my anagrams at.py
@@ -24,7 +24,6 @@ def check(s1, s2):
 	if(sorted(s1)== sorted(s2)):
 		return s2
 
-
 def anagrams(word, words):
       result = []
       for i in words:
@@ -33,7 +32,14 @@ def anagrams(word, words):
       return result
 
 
+
 # 2
+def anagrams(word, words): 
+      return [item for item in words if sorted(item)==sorted(word)]
 
-def anagrams(word, words): return [item for item in words if sorted(item)==sorted(word)]
 
+
+
+#3
+def anagrams(word, words):
+    return filter(lambda x: sorted(word) == sorted(x), words)
